e26c788d9ea78a6293bde0aa9ba727f041cedaa2
Igor <iwebwork.adm@gmail.com>
2021-03-11 22:54:45 -0300

    Exemplo de função procedural inserida no codigo e tambem comentarios

--- src/components/Mega/MegaSena.js
+++ src/components/Mega/MegaSena.js
@@ -14,11 +14,33 @@ export default class Mega extends Component{
         this.setState({qtDeNumeros: +qt})
     }
 
-    gerarNumeros = () =>{
+    gerarNumerosFunc = () =>{
+        // Gerar um array 
         const numeros = Array(this.state.qtDeNumeros)
+            // com a quantidade de numeros da classe
             .fill()
+            // O reduce cria um array com a quantidade de numeros definido
+            // chamando a função de gerar um numero que seja diferente dos que ja existem
             .reduce(n => [...n, this.gerarNumero(n)], [])
+            // O sorte organiza o arrray, neste caso é do menor para o maior
             .sort((a, b) => a - b)
+        
+        // Depois do array gerado eu entrego para o state
+        this.setState({numeros})
+    }
+
+    gerarNumerosProc = () => {
+        const {qtDeNumeros} = this.state
+        const numeros = []
+
+        for (let index = 0; index < qtDeNumeros; index++) {
+            const n = this.gerarNumero(numeros)
+
+            // Dou um push no mesmo array que estou gerando
+            numeros.push(n)
+        }
+
+        numeros.sort((a,b) => a - b)
         this.setState({numeros})
     }
 
@@ -26,7 +48,7 @@ export default class Mega extends Component{
         const numero = getRandomIntInclusive(0, this.state.qtDeNumeros * 1000)
 
         //Caso o numero gerado ja exista dentro do nums, eu chamo a função novamente
-        return nums.includes(numero) ? this.gerarNumero(nums) : numero
+        return nums.includes(numero) ? this.gerarNumerosFunc(nums) : numero
 
     }
 
@@ -46,7 +68,7 @@ export default class Mega extends Component{
 
                 <Button
                     title="Gerar Numeros"
-                    onPress={this.gerarNumeros}
+                    onPress={this.gerarNumerosProc}
                 />
 
                 <Text>
